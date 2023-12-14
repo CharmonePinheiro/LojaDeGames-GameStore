1603ae685a01b032c2ffa53bbd00d706d2b9e35e
Your Name <charmonepinheirorodrigues@gmail.com>
2023-12-14 13:18:01 -0300

    configurações iniciais do projeto

---
+++ LojaDeGames-GameStore/src/main/java/com/lojadegames/demo/LojaDeGamesGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lojadegames.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGamesGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGamesGameStoreApplication.class, args);
+	}
+
+}
---
+++ LojaDeGames-GameStore/src/test/java/com/lojadegames/demo/LojaDeGamesGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojadegames.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
